e6225236aa6cbe51d22ccc26de4fbff5500c3b3b
Manuel Valles <manukempo@gmail.com>
2020-10-05 13:06:05 +0100

    Launches implementation: GetAll & GetOne

--- src/api.ts
+++ src/api.ts
@@ -1,16 +1,33 @@
-import { Context, Router } from 'https://deno.land/x/oak@v6.3.0/mod.ts';
+import { Router, RouterContext } from 'https://deno.land/x/oak@v6.3.0/mod.ts';
 import * as planets from './models/planets.ts'
+import * as launches from './models/launches.ts'
 
 const router: Router = new Router()
 
-router.get('/', (ctx: Context) => {
+router.get('/', (ctx: RouterContext): void => {
     ctx.response.body = 'Hello Oak!'
 })
 
-router.get('/planets', (ctx: Context) => {
+router.get('/planets', (ctx: RouterContext): void => {
     // Example specific error - Oak won't show the message as body for 500 statuses
     // ctx.throw(501, "Sorry planets aren't available!")
     ctx.response.body = planets.getAllPlanets()
+})
+
+router.get('/launches', (ctx: RouterContext): void => {
+    ctx.response.body = launches.getAll()
+})
+
+router.get('/launches/:id', (ctx: RouterContext): void => {
+    const id = ctx.params?.id
+    if (id) {
+        const launchesList = launches.getOne(Number(id))
+        if (launchesList) {
+            ctx.response.body = launchesList
+        } else {
+            ctx.throw(400, "Launch with that ID doesn't exist")
+        }
+    }
 })
 
 export default router
---
+++ src/models/launches.ts
@@ -0,0 +1,59 @@
+import * as log from 'https://deno.land/std/log/mod.ts';
+import * as _ from 'https://deno.land/x/lodash@4.17.15-es/lodash.js';
+
+interface Launch {
+    flightNumber: number
+    mission: string
+    rocket: string
+    customers: string[]
+    launchDate: number
+    upcoming: boolean
+    success?: boolean
+    target?: string
+}
+
+interface Payload {
+    customers: string[]
+}
+
+const launches = new Map<number, Launch>()
+
+export const downloadLaunchData = async (): Promise<void> => {
+    log.info('Downloading launch data...')
+    const response: Response = await fetch('https://api.spacexdata.com/v3/launches', {
+        method: 'GET'
+    })
+
+    if (!response.ok) {
+        log.warning('Problem downloading launch data')
+        throw new Error('Launch data download failed.')
+    }
+
+    const launchData = await response.json()
+    for (const launch of launchData) {
+        const payloads: Payload[] = launch.rocket.second_stage.payloads
+        const customers: string[] = _.flatMap(payloads, (payload: Payload) => payload.customers)
+        const flightData: Launch = {
+            flightNumber: launch.flight_number,
+            mission: launch.mission_name,
+            rocket: launch.rocket.rocket_name,
+            customers,
+            launchDate: launch.launch_date_unix,
+            upcoming: launch.upcoming,
+            success: launch.launch_success
+        }
+
+        launches.set(flightData.flightNumber, flightData)
+        log.info(JSON.stringify(flightData))
+    }
+}
+
+
+await downloadLaunchData()
+log.info(`Downloaded data for ${launches.size} SpaceX launches.`)
+
+export const getAll = (): Launch[] => Array.from(launches.values())
+
+export const getOne = (id: number): Launch | undefined => {
+    if (launches.has(id)) return launches.get(id)
+}
